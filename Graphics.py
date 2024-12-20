e26b7c3ffd927f9d07bc8f8fa30c7233c21a1092
IgloBruh <DmShadow83@Gmail.com>
2024-12-20 19:13:14 +0200

    Added "Graphics" class to hold all sprites

---
+++ Graphics.py
@@ -0,0 +1,23 @@
+from tkinter import PhotoImage
+
+class Graphics:
+    def __init__(self):
+
+        # Pawns
+        self.pawnWhite = PhotoImage(file="Graphics\Pawn_White.png")
+        self.pawnBlack = PhotoImage(file="Graphics\Pawn_Black.png")
+        # Knights
+        self.knightWhite = PhotoImage(file="Graphics\Knight_White.png")
+        self.knightWhite = PhotoImage(file="Graphics\Knight_Black.png")
+        # Bishops
+        self.knightWhite = PhotoImage(file="Graphics\Bishop_White.png")
+        self.knightWhite = PhotoImage(file="Graphics\Bishop_Black.png")
+        # Rooks
+        self.knightWhite = PhotoImage(file="Graphics\Rook_White.png")
+        self.knightWhite = PhotoImage(file="Graphics\Rook_Black.png")
+        # Queens
+        self.knightWhite = PhotoImage(file="Graphics\Queen_White.png")
+        self.knightWhite = PhotoImage(file="Graphics\Queen_Black.png")
+        # King
+        self.knightWhite = PhotoImage(file="Graphics\King_White.png")
+        self.knightWhite = PhotoImage(file="Graphics\King_Black.png")
