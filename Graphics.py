71476ed963cd1fb8ff801976a1598aba24235082
IgloBruh <DmShadow83@Gmail.com>
2024-12-22 13:11:51 +0200

    Graphics class replaced with a dictionary

--- Graphics.py
+++ Graphics.py
@@ -1,23 +1,16 @@
 from tkinter import PhotoImage
 
-class Graphics:
-    def __init__(self):
-
-        # Pawns
-        self.pawnWhite = PhotoImage(file="Graphics\Pawn_White.png")
-        self.pawnBlack = PhotoImage(file="Graphics\Pawn_Black.png")
-        # Knights
-        self.knightWhite = PhotoImage(file="Graphics\Knight_White.png")
-        self.knightWhite = PhotoImage(file="Graphics\Knight_Black.png")
-        # Bishops
-        self.knightWhite = PhotoImage(file="Graphics\Bishop_White.png")
-        self.knightWhite = PhotoImage(file="Graphics\Bishop_Black.png")
-        # Rooks
-        self.knightWhite = PhotoImage(file="Graphics\Rook_White.png")
-        self.knightWhite = PhotoImage(file="Graphics\Rook_Black.png")
-        # Queens
-        self.knightWhite = PhotoImage(file="Graphics\Queen_White.png")
-        self.knightWhite = PhotoImage(file="Graphics\Queen_Black.png")
-        # King
-        self.knightWhite = PhotoImage(file="Graphics\King_White.png")
-        self.knightWhite = PhotoImage(file="Graphics\King_Black.png")
+sprites = {
+    'pawn_w' : PhotoImage(file="Graphics/Pawn_White.png"),
+    'pawn_b' : PhotoImage(file="Graphics/Pawn_Black.png"),
+    'knight_w' : PhotoImage(file="Graphics/Knight_White.png"),
+    'knight_b' : PhotoImage(file="Graphics/Knight_Black.png"),
+    'bishop_w' : PhotoImage(file="Graphics/Bishop_White.png"),
+    'bishop_b' : PhotoImage(file="Graphics/Bishop_Black.png"),
+    'rook_w' : PhotoImage(file="Graphics/Rook_White.png"),
+    'rook_b' : PhotoImage(file="Graphics/Rook_Black.png"),
+    'queen_w' : PhotoImage(file="Graphics/Queen_White.png"),
+    'queen_b' : PhotoImage(file="Graphics/Queen_Black.png"),
+    'king_w' : PhotoImage(file="Graphics/King_White.png"),
+    'king_b' : PhotoImage(file="Graphics/King_Black.png")
+}
